a4f346f7f529cca8e492a2b5ad215924b4da821a
abdelrahmantaymour <abdalrahman.m.taymor@gmail.com>
2026-08-27 10:52:51 +0300

    feat(Dashboard): Refactor dashboard components and implement recent bookings and stays hooks

---
+++ src/features/dashboard/DashboardLayout.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
+
+const StyledDashboardLayout = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: auto 34rem auto;
+  gap: 2.4rem;
+`;
+
+function DashboardLayout() {
+  const { bookings, isPending: isLoading1 } = useRecentBookings();
+  const { isPending: isLoading2, stays, confirmedStays } = useRecentStays();
+
+  if (isLoading1 || isLoading2) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
