fafcf733e27b8b905f71a3ad79671b9e05c25a53
abdelrahmantaymour <abdalrahman.m.taymor@gmail.com>
2026-08-28 11:33:06 +0300

    fix(LoginForm): reset email and password state to empty strings

--- src/features/authentication/LoginForm.tsx
+++ src/features/authentication/LoginForm.tsx
@@ -9,8 +9,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("taymour@test.com");
-  const [password, setPassword] = useState("Password123");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isPending: isLoading } = useLogin();
 
   function handleSubmit(e: SubmitEvent<HTMLFormElement>) {
