b6af27490056e3838c2499c291da3a8b4a9923d8
abdelrahmantaymour <abdalrahman.m.taymor@gmail.com>
2026-08-28 09:10:14 +0300

    feat(DarkModeContext): initialize dark mode state based on system preference feat(BookingDetail): add Empty component for handling no booking state refactor(Sidebar): remove unused Uploader component

--- src/context/DarkModeContext.tsx
+++ src/context/DarkModeContext.tsx
@@ -4,7 +4,10 @@ import { useLocalStorageState } from "../hooks/useLocalStorageState";
 const DarkModeContext = createContext({});
 
 function DarkModeProvider({ children }: { children: ReactNode }) {
-  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(
+    window.matchMedia("(prefers-color-scheme: dark)").matches,
+    "isDarkMode",
+  );
 
   useEffect(
     function () {
